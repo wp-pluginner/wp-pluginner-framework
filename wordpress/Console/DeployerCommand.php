e1f1205969d77715cf89f5513cd3ec7f10f0f996
Pijar Aji Pratama <pijars@Pijars-MacBook-Pro.local>
2018-08-30 01:20:20 +0700

    ngrapihin lagi

--- wordpress/Console/DeployerCommand.php
+++ wordpress/Console/DeployerCommand.php
@@ -25,21 +25,63 @@ class DeployerCommand extends Command
 
         $this->config = wp_pluginner_config('deployer');
 
-        $this->deployPath = wp_pluginner_base_path('production/' . $this->name . time());
-        $this->processDirectory($this->basePath);
-        $this->processIncludes();
+
+        $this->deployPath = wp_pluginner_base_path('production/' . $this->name);
+        if ($this->files->exists($this->deployPath)) {
+            $this->error('Name already used, please check directory: production/' . $this->name);
+        } else {
+            $this->files->makeDirectory($this->deployPath, 0777, true);
+
+            $this->processCopyDirectory($this->basePath);
+            $this->processCopyIncludes();
+
+            if (is_array($this->config['rename']) && !empty($this->config['rename'])) {
+                $rename = $this->config['rename'];
+                $this->config['rename'] = [
+                    'from' => array_keys($rename),
+                    'to' => array_values($rename)
+                ];
+                $this->processRenameDirectory($this->deployPath);
+            }
+            $composerUpdate = 'cd ' . $this->deployPath . ' && composer dump-autoload';
+            exec($composerUpdate);
+
+        }
     }
 
-    protected function processDirectory($path)
+    protected function processRenameDirectory($path)
     {
-        if ($this->files->isDirectory($path) && $this->isFileOrDirInclude($path)) {
-            if ($this->isRequireDepth($path)) {
+        if ($this->files->isDirectory($path)) {
+            $files = $this->files->glob($path . '/{,.}[!.,!..]*', GLOB_BRACE);
+            foreach ($files as $file) {
+                if ($this->files->isFile($file)) {
+                    $this->processRenameFile($file);
+                } else {
+                    $this->processRenameDirectory($file);
+                }
+            }
+        }
+    }
+
+    protected function processRenameFile($path)
+    {
+        if ($this->files->exists($path) && $this->files->isFile($path)) {
+            $content = $this->files->get($path);
+            $content = str_replace($this->config['rename']['from'], $this->config['rename']['to'], $content);
+            $this->files->put($path, $content);
+        }
+    }
+
+    protected function processCopyDirectory($path)
+    {
+        if ($this->files->isDirectory($path) && $this->isFileOrDirIncludeForCopy($path)) {
+            if ($this->isRequireDepthForCopy($path)) {
                 $files = $this->files->glob($path . '/{,.}[!.,!..]*', GLOB_BRACE);
                 foreach ($files as $file) {
                     if ($this->files->isFile($file)) {
-                        $this->processFile($file);
+                        $this->processCopyFile($file);
                     } else {
-                        $this->processDirectory($file);
+                        $this->processCopyDirectory($file);
                     }
                 }
             } else {
@@ -54,54 +96,58 @@ class DeployerCommand extends Command
 
     protected function copyFile($path)
     {
-        $source = $path;
-        $target = Str::replaceFirst($this->basePath, $this->deployPath, $path);
-        $this->files->copy($source, $target);
+        if ($this->files->exists($path) && $this->files->isFile($path)) {
+            $target = Str::replaceFirst($this->basePath, $this->deployPath, $path);
+            $dir = $this->files->dirname($target);
+            if (!$this->files->exists($dir)) {
+                $this->files->makeDirectory($dir, 0777, true);
+            }
+            $this->files->copy($path, $target);
+        }
     }
 
     protected function copyDirectory($path)
     {
-        $source = $path;
-        $target = Str::replaceFirst($this->basePath, $this->deployPath, $path);
-        $this->files->copyDirectory($source, $target);
+        if ($this->files->exists($path) && $this->files->isDirectory($path)) {
+            $target = Str::replaceFirst($this->basePath, $this->deployPath, $path);
+            $this->files->copyDirectory($path, $target);
+        }
     }
 
-    protected function processIncludes()
+    protected function processCopyIncludes()
     {
-        if (is_array($this->config['includes'])) {
-            foreach ($this->config['includes'] as $include) {
-                if ($this->files->exists($include)) {
-                    if ($this->files->isFile($include)) {
-                        $this->processFile($include);
-                    } else {
-                        $this->processDirectory($include);
-                    }
+        if (is_array($this->config['files']['includes'])) {
+            foreach ($this->config['files']['includes'] as $include) {
+                if ($this->files->isFile($include)) {
+                    $this->processCopyFile($include);
+                } else {
+                    $this->processCopyDirectory($include);
                 }
             }
         }
     }
 
-    protected function processFile($path)
+    protected function processCopyFile($path)
     {
-        if ($this->files->isFile($path) && $this->isFileOrDirInclude($path)) {
+        if ($this->isFileOrDirIncludeForCopy($path)) {
             $this->copyFile($path);
         }
     }
 
-    protected function isFileOrDirInclude($path)
+    protected function isFileOrDirIncludeForCopy($path)
     {
-        if (is_array($this->config['excludes'])) {
-            if(in_array($path, $this->config['excludes'])) {
+        if (is_array($this->config['files']['excludes'])) {
+            if(in_array($path, $this->config['files']['excludes'])) {
                 return false;
             }
         }
         return true;
     }
 
-    protected function isRequireDepth($path)
+    protected function isRequireDepthForCopy($path)
     {
-        if (is_array($this->config['excludes'])) {
-            foreach ($this->config['excludes'] as $exclude) {
+        if (is_array($this->config['files']['excludes'])) {
+            foreach ($this->config['files']['excludes'] as $exclude) {
                 if (Str::contains($this->files->dirname($exclude), $path)) {
                     return true;
                 }
